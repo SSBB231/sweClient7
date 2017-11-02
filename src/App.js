b0513e390f12017a73f96bd0e083a213f6ea8eab
Jimmy D. Bodden Pineda <jbjdbodden2@gmail.com>
2017-11-01 21:41:38 -0400

    Updating server.js

--- src/App.js
+++ src/App.js
@@ -34,9 +34,81 @@ class App extends Component
                     {/*</p>*/}
                     <HomeScreen onClick={this.changeToUsers}/>
                     <AllUsers/>
+                    <AllAppointments/>
                 </div>
             );
         }
+    }
+}
+
+class AllAppointments extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {size: 2};
+        this.userarray = [];
+        //let firstnames userarray.map((firstname
+        this.firstnames = [1, 2, 3];
+        this.lastnames = [1, 2, 3];
+        this.arrayemails = [3, 2, 1];
+    }
+
+    componentDidMount() {
+
+        fetch('/users/cdione/appointments/')
+            .then(response => {
+                return response.json();
+            })
+            .then((json) => {
+                this.userarray = json;
+                console.log("FETCHED APPOINTMENTS");
+                console.log(this.userarray);
+                this.firstnames = this.userarray.map((user) => user.name);
+                this.lastnames = this.userarray.map((user) => user.lastName);
+
+                this.arrayemails = this.userarray.map((user) => user.email);
+                this.setState({fetched: "true"});
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+    }
+
+    render() {
+        let rows = [];
+        let count = 0;
+        for (var i = 0; i < this.userarray.length/2; i++) {
+            let rowID = `row${i}`
+            let cell = []
+
+            for (var idx = 0; idx < 2; idx++) {
+                let cellID = `cell${i}-${idx}`
+                if(count%2==0)
+                    cell.push(<td key={cellID} id={cellID}>{this.firstnames[count]+" "+this.lastnames[count]}</td>)
+                else
+                    cell.push(<td key={cellID} id={cellID}>{this.arrayemails[count-1]}</td>)
+
+                count++;
+                console.log(this.firstnames);
+            }
+            rows.push(<tr key={i} id={rowID}>{cell}</tr>)
+        }
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col s12 board">
+                        <table id="simple-board">
+                            <tbody>
+                            {rows}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        )
+
     }
 }
 
